e632a8380338cad62baa7e8ce040366de1196272
Gosling7 <gaska.michal7@gmail.com>
2025-02-11 23:04:12 +0100

    wip: integration tests

---
+++ tests/FormulaOne.Tests.Integration/Repositories/TeamRepositoryTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testcontainers.MsSql;
+using Testcontainers.Xunit;
+using Xunit.Abstractions;
+
+namespace FormulaOne.Tests.Integration.Repositories;
+
+public class TeamRepositoryTests : ContainerFixture<MsSqlBuilder, MsSqlContainer>
+{
+    public TeamRepositoryTests(IMessageSink messageSink) : base(messageSink)
+    {
+    }
+
+    protected override MsSqlBuilder Configure(MsSqlBuilder builder)
+    {
+        return base.Configure(builder);
+    }
+
+    [Fact]
+    public void GetItemsAsync_should()
+    {
+
+    }
+}
